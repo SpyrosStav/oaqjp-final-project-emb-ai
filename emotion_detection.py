43e465d5cf8026e3f330bdd1720e6867d3cec9a9
SpyrosStav <spyrosstav99@gmail.com>
2025-06-18 08:55:02 -0400

    Adds more functionality

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,5 +6,7 @@ def emotion_detector(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, headers = header, json = input_json)
-    data = json.loads(response.text)
+    data = json.loads(response.text)['emotionPredictions'][0]['emotion']
+    max_key = max(data, key = data.get)
+    data['dominant_emotion'] = max_key
     return data
