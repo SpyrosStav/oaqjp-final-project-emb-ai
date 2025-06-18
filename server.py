9748cb1138c5025af9c5b65f3c45ce9562437a0b
SpyrosStav <spyrosstav99@gmail.com>
2025-06-18 10:02:47 -0400

    adds server

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask(__name__)
+
+@app.route('/emotionDetector')
+def detect_emotion():
+    
+    text_to_analyse = request.args.get('textToAnalyze')
+    result = emotion_detector(text_to_analyse)
+
+    answer = f'''For the given statement, the system response is
+    'anger' {result['anger']}, 'disgust' {result['disgust']}, 
+    'fear' {result['fear']}, 'joy' {result['joy']} and
+    'sadness' {result['sadness']}. The dominant emotion is
+    {result['dominant_emotion']}.
+    '''
+    return answer
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0', port = 5000)
