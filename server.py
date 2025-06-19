ce29576e160066902eb0d3e4470efe3b07ccfcb7
SpyrosStav <spyrosstav99@gmail.com>
2025-06-19 06:51:18 -0400

    modified for pylint

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+''''This module runs a Flask web app that performs emotion analysis on given sentences'''
+
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,24 +7,25 @@ app = Flask(__name__)
 
 @app.route('/emotionDetector')
 def detect_emotion():
-    
+    '''Calls to emotion_detector function to perform analysis and returns the result'''
     text_to_analyse = request.args.get('textToAnalyze')
     result = emotion_detector(text_to_analyse)
 
-    if(result['dominant_emotion'] == None):
+    if result['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
-    else:
-        answer = f'''For the given statement, the system response is
-        'anger' {result['anger']}, 'disgust' {result['disgust']}, 
-        'fear' {result['fear']}, 'joy' {result['joy']} and
-        'sadness' {result['sadness']}. The dominant emotion is
-        {result['dominant_emotion']}.
-        '''
+
+    answer = f'''For the given statement, the system response is
+    'anger' {result['anger']}, 'disgust' {result['disgust']}, 
+    'fear' {result['fear']}, 'joy' {result['joy']} and
+    'sadness' {result['sadness']}. The dominant emotion is
+    {result['dominant_emotion']}.
+    '''
     return answer
 
 
 @app.route('/')
 def index():
+    '''returns the index'''
     return render_template('index.html')
 
 if __name__ == "__main__":
